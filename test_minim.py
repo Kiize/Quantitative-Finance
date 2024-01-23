6f9e19ff57939f513fde320edb3cdb4a48d124b6
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2024-01-23 21:18:55 +0100

    A few changes to make the code clearer

--- test_minim.py
+++ test_minim.py
@@ -1,63 +1,83 @@
+"""
+Code to test and understand how scipy.optimize.minimize works
+"""
 import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 
-# Test 1D libero.
 
-# Function to minimize.
-"""
+#=======================================================================
+# Unconstrained minimization test in 1D
+#=======================================================================
+
 def f(x):
+    ''' Function to minimize.
+    '''
     return x**2 + np.sin(x) - np.cosh(np.sqrt(np.abs(x)))
 
-# Find the minimum.
-
-result = opt.minimize(f, x0 = -2)
-
-x_plot = np.linspace(-1, 1, 1000)
-plt.plot(x_plot, f(x_plot))
-plt.plot(result.x, result.fun, marker = 'o')
+# Find the minimum; x0 is an init point
+result = opt.minimize(f, x0=-2)
 print(result)
+
+# Plot result
+plt.figure(1)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title("Unconstrained minimization test in 1D")
+x_plot = np.linspace(-1, 1, 1000)
+plt.plot(x_plot, f(x_plot), 'b', label="function")
+plt.plot(result.x, result.fun, "ro", label="minimum")
+plt.legend(loc='best')
+plt.grid()
 plt.show()
-"""
 
-# Test 2D con constraint.
 
-# Function to minimize.
+#=======================================================================
+# Constrained minimization test in 2D
+#=======================================================================
 
-def f(vec, param = 1):
+
+def f(vec, param=1):
+    ''' Function to minimize.
+    '''
     x, y = vec
     return param*x + y + x**2
 
-# Constraint.
-
 def g(vec):
+    ''' Constraint; write as g(x) = 0
+    '''
     x, y = vec
     return x + y - 1 
 
-
 # Define the constraints in the form required by the minimize function.
-
 cons = ({'type': 'eq', 'fun': g})
 
 # Set the initial guess for the optimization
 x0 = np.array([3, -5])
 
 # Minimize the objective function subject to the constraints
-param = 10
-result = opt.minimize(f, x0, constraints=cons, args=(-param))
+param = -10
+result = opt.minimize(f, x0, constraints=cons, args=(param))
+print(result)
 
-# Plot.
-
-x_plot = np.linspace(-2*param, 2*param, 100)
+# Plot
+x_plot = np.linspace(-2*abs(param), 2*abs(param), 100)
 X, Y = np.meshgrid(x_plot, x_plot)
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.plot_surface(X, Y, f([X, Y]), alpha = 0.4)
-ax.plot(*result.x, f(result.x), marker = 'o')
-ax.plot(x_plot, 1 - x_plot, f([x_plot, 1 - x_plot]), color = 'orange')
+fig = plt.figure(2)
+ax  = fig.add_subplot(projection='3d')
+ax.set_title("Constrained minimization test in 2D")
+ax.set_ylabel("y")
+ax.set_xlabel("x")
+ax.set_zlabel("F(x,y)")
+
+#plot
+surf=ax.plot_surface(X, Y, f([X, Y], param), color='blue', alpha=0.4, label="function")
+ax.plot(*result.x, f(result.x, param), "ko", label="minimum")
+ax.plot(x_plot, 1 - x_plot, f([x_plot, 1 - x_plot], param), color='orange', label="Constraint")
+
+#legend
+surf._facecolors2d = surf._facecolor3d # to avoid problem
+surf._edgecolors2d = surf._edgecolor3d # with the legend
+ax.legend()
 plt.show()
-
-
-
-
