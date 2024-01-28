640cb8034bdf63292db013d0fdd806382cf0712e
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2024-01-29 00:06:49 +0100

    all code in a class

--- efficient_frontier.py
+++ efficient_frontier.py
@@ -12,278 +12,205 @@ import pandas as pd
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 
-days_in_yr = 252 # day of activity
 
-#==============================================================================
-# Portfolio creation
-#==============================================================================
-
-def portfolio(w, returns, ret_cov):
+class Portfolio:
     '''
-    Returns the mean and standard deviation of returns for a portfolio
-    
-    Parameter
-    ---------
-    w : 1darray
-        weights of portfolio
-    returns : 2darray
-        matrix of return N asset for L time
-    ret_cov : 2darray
-        corss correlation matrix N x N
-    
-    Returns
-    -------
-    mean_prt : float
-        mean of portfolio return
-    devs_prt : float
-        standard deviation of portfolio 
+    Class to create optimal portfolio according
+    Markowitz theory of portfolio optimization
     '''
-    mean_ret = np.mean(returns, axis=1)
-    mean_prt = sum( w * mean_ret) * days_in_yr
-    devs_prt = np.sqrt((w.T @ ret_cov @ w) * days_in_yr)
-    
-    return mean_prt, devs_prt
+
+    def __init__(self, returns, ret_cov, ticker, risk_free_rate=0, days_in_yr=252):
+        '''
+        returns : 2darray
+            matrix of return N asset for L time
+        ret_cov : 2darray
+            corss correlation matrix N x N
+        ticker : list
+            list of ticker we want include in our portfolio
+        risk_free_rate : float, optional, default 0
+            The risk-free rate is the rate of return on an investment
+            that has a zero chance of loss. It means the investment 
+            is so safe that there is no risk associated with it.
+        days_in_yr : int, optionale, default 252
+            days of financial market activity
+        '''
+        self.returns        = returns
+        self.ret_cov        = ret_cov
+        self.risk_free_rate = risk_free_rate
+        self.days_in_yr     = days_in_yr
+        self.ticker         = ticker
+ 
+
+    def ret_portfolio(self, w):
+        '''
+        Function to compute the return of the porfolio.
+        
+        Parameter
+        ---------
+        w : 1darray
+            weights of portfolio
+
+        Return
+        ------
+        ret_port : float
+            return of the portfolio
+        '''
+        ret_port = np.mean(self.returns, axis=1)
+        ret_port = sum( w * ret_port) * self.days_in_yr
+        return ret_port
 
 
-def negative_sharpe_ratio(w, returns, ret_cov, risk_free_rate=0.):
-    '''
-    Function to compute the sharpe ratio, and we want to maximize it
-    so we add a minus and then minimize th negative sharpe ratio.
-    
-    Parameter
-    ---------
-    w : 1darray
-        weights of portfolio
-    returns : 2darray
-        matrix of return N asset for L time
-    ret_cov : 2darray
-        corss correlation matrix N x N
-    risk_free_rate : float, optional, default 0
-        The risk-free rate is the rate of return on an investment
-        that has a zero chance of loss. It means the investment 
-        is so safe that there is no risk associated with it.
-    
-    Returns
-    -------
-    sharpe_ratio : float
-        It measures the additional return above the
-        return of the risk-free asset per unit of volatility
-    '''
-    ret_prt, vol_prt = portfolio(w, returns, ret_cov)
-    sharpe_ratio = (ret_prt - risk_free_rate)/vol_prt 
-
-    return -sharpe_ratio
+    def vol_portfolio(self, w):
+        '''
+        Function to compute the volatility of the porfolio.
+        
+        Parameter
+        ---------
+        w : 1darray
+            weights of portfolio
+        
+        Return
+        ------
+        vol_port : float
+            volatility of the portfolio
+        '''
+        vol_port = np.sqrt((w.T @ self.ret_cov @ w) * self.days_in_yr)
+        return vol_port
 
 
-def max_sharpe_ratio(returns, ret_cov, risk_free_rate=0.):
-    '''
-    Function that maximize the sharpe ratio,
-    to find the optimal weights for our portfolio
-    
-    Parameter
-    ---------
-    returns : 2darray
-        matrix of return N asset for L time
-    ret_cov : 2darray
-        corss correlation matrix N x N
-    risk_free_rate : float, optional, default 0
-        The risk-free rate is the rate of return on an investment
-        that has a zero chance of loss. It means the investment 
-        is so safe that there is no risk associated with it.
-    
-    Returns
-    -------
-    result : dict
-        result of minimizzation
-    '''
-    # Number of asset in oru portfolio
-    n = returns.shape[0]
-    # Extra argumento to pass to the objective function
-    args = (returns, ret_cov, risk_free_rate)
-    # Unitarity constraint
-    constraints = ({'type': 'eq', 'fun': lambda w: np.sum(w) - 1.})
-    # All weights must be between 0 and 1, they are percentages
-    bounds = tuple((0, 1) for _ in range(n))
-    # Initial guess
-    init = n * [1 / n]
-    # minimizzation
-    result = opt.minimize(negative_sharpe_ratio, init, args=args,
-                          bounds=bounds, constraints=constraints)
-    return result
+    def tot_portfolio(self, w):
+        '''
+        Function to compute the whole portfolio information
+        
+        Parameter
+        ---------
+        w : 1darray
+            weights of portfolio
+        
+        Return
+        ------
+        mu : float
+            return of the portfolio
+        std : float
+            volatility of the portfolio
+        '''
+        mu  = self.ret_portfolio(w)
+        std = self.vol_portfolio(w)
+        return mu, std
 
 
-def portfolio_ret(w, returns, ret_cov):
-    '''
-    Returns the mean and standard deviation of returns for a portfolio
-    
-    Parameter
-    ---------
-    w : 1darray
-        weights of portfolio
-    returns : 2darray
-        matrix of return N asset for L time
-    ret_cov : 2darray
-        corss correlation matrix N x N
-    
-    Returns
-    -------
-    mean_prt : float
-        mean of portfolio return
-    '''
-    return portfolio(w, returns, ret_cov)[0]
+    def negative_sharpe_ratio(self, w):
+        '''
+        Function to compute the sharpe ratio, and we want to maximize it
+        so we add a minus and then minimize th negative sharpe ratio.
+
+        Parameter
+        ---------
+        w : 1darray
+            weights of portfolio
+
+        Returns
+        -------
+        sharpe_ratio : float
+            It measures the additional return above the
+            return of the risk-free asset per unit of volatility
+        '''
+
+        ret_prt, vol_prt = self.tot_portfolio(w)
+        sharpe_ratio = (ret_prt - self.risk_free_rate)/vol_prt 
+
+        return -sharpe_ratio
 
 
-def portfolio_vol(w, returns, ret_cov):
-    '''
-    Returns the mean and standard deviation of returns for a portfolio
-    
-    Parameter
-    ---------
-    w : 1darray
-        weights of portfolio
-    returns : 2darray
-        matrix of return N asset for L time
-    ret_cov : 2darray
-        corss correlation matrix N x N
-    
-    Returns
-    -------
-    devs_prt : float
-        standard deviation of portfolio 
-    '''
-    return portfolio(w, returns, ret_cov)[1]
+    def minimum(self, f, constraints, args=()):
+        '''
+        Function to compute the optimal weights via optimizzatio.
+        Although it is possible to change the constraints,
+        here we consider the bounds and the starting point
+        he same for every situation.
+
+        Parameters
+        ----------
+        f : callable
+            function to minimize, 
+            portfolio's volatility or negative sharpe ratio
+        constraints : tuple of dictionarys
+            all contraints for minimization, e.g. unitary weights
+            and fixed portfolio's return
+        args : tuple
+            extra argument to pass at f
+
+        Return
+        ------
+        result : dict
+            result of minimization
+        '''
+        # Number of asset in our portfolio
+        n = self.returns.shape[0]
+        # All weights must be between 0 and 1, they are percentages
+        bounds = tuple((0, 1) for _ in range(n))
+        # Initial guess
+        init = n * [1 / n]
+        # minimizzation
+        result = opt.minimize(f, init, args=args, bounds=bounds,
+                              constraints=constraints)
+        return result
 
 
-def min_vol(returns, ret_cov):
-    '''
-    Function that minimize the portfolio's volatility,
-    to find the optimal weights.
-    
-    Parameter
-    ---------
-    returns : 2darray
-        matrix of return N asset for L time
-    ret_cov : 2darray
-        corss correlation matrix N x N
-    
-    Returns
-    -------
-    result : dict
-        result of minimizzation
-    '''
-    # Number of asset in oru portfolio
-    n = returns.shape[0]
-    # Extra argumento to pass to the objective function
-    args = (returns, ret_cov)
-    # Unitarity constraint
-    constraints = ({'type': 'eq', 'fun': lambda w: np.sum(w) - 1.})
-    # All weights must be between 0 and 1, they are percentages
-    bounds = tuple((0, 1) for _ in range(n))
-    # Initial guess
-    init = n * [1 / n]
-    # minimizzation
-    result = opt.minimize(portfolio_vol, init, args=args,
-                          bounds=bounds, constraints=constraints)
-    return result
+    def optimization(self, N_P):
+        '''
+        Function to compute all efficient frontier
 
+        Parameter
+        ---------
+        N_P : int
+            number of points, i.e. nuber of portfolios
 
-def frontier(returns, ret_cov, target):
-    '''
-    Function that minimize the portfolio's volatility,
-    to find the optimal weights.
-    
-    Parameter
-    ---------
-    returns : 2darray
-        matrix of return N asset for L time
-    ret_cov : 2darray
-        corss correlation matrix N x N
-    target : float
-        expected return
-    
-    Returns
-    -------
-    result : dict
-        result of minimizzation
-    '''
-    # Number of asset in oru portfolio
-    n = returns.shape[0]
-    # Extra argumento to pass to the objective function
-    args = (returns, ret_cov)
-    # Unitarity constraint and return constraint
-    constraints = ({'type': 'eq', 'fun': lambda w: np.sum(w) - 1.},
-                   {'type': 'eq', 'fun': lambda w: portfolio_ret(w, returns, ret_cov) - target})
-    # All weights must be between 0 and 1, they are percentages
-    bounds = tuple((0, 1) for _ in range(n))
-    # Initial guess
-    init = n * [1 / n]
-    # minimizzation
-    result = opt.minimize(portfolio_vol, init, args=args,
-                          bounds=bounds, constraints=constraints)
-    return result
+        Returns
+        -------
+        frontier_vol : 1darray
+            result of optimizzation for volatility (x-axis)
+        target : 1darray
+            return that we want for our potfolio (y-axis)
+        data : pandas DataFrame
+            DataFrame with optimal weights and associated ticker
+        '''
+        # Frontier array
+        frontier_vol = np.zeros(N_P)
+        # Dataframe to store all porfolios
+        data = pd.DataFrame(index=self.ticker, columns=[f"{i}" for i in range(N_P)])
+        # Unitarity constraint
+        constraints = ({'type': 'eq', 'fun': lambda w: np.sum(w) - 1.})
 
+        # Maximization of sharpe ratio
+        result_sr = self.minimum(self.negative_sharpe_ratio, constraints)
+        ret_prt_sr, vol_prt_sr = self.tot_portfolio(result_sr['x'])
+        # Minimization of volatility
+        result_mv = self.minimum(self.vol_portfolio, constraints)
+        ret_prt_mv, vol_prt_mv = self.tot_portfolio(result_mv['x'])
 
-def get_result(returns, ret_cov, ticker, N_P=20, risk_free_rate=0.):
-    '''
-    Function tu compute the return of the portfolio
-    and save in a pandas dataframe the result.
-    
-    Parameter
-    ---------
-    returns : 2darray
-        matrix of return N asset for L time
-    ret_cov : 2darray
-        corss correlation matrix N x N
-    ticker : list
-        list of ticker of our portfolio
-    N_P : int, optinal, default 20
-        number of portfolios on the efficient frontier
-    risk_free_rate : float, optional, default 0
-        The risk-free rate is the rate of return on an investment
-        that has a zero chance of loss. It means the investment 
-        is so safe that there is no risk associated with it.
-    
-    Returns
-    -------
-    frontier_vol : 1darray
-        result of optimizzation for volatility (x-axis)
-    target : 1darray
-        return that we want for our potfolio (y-axis)
-    data : pandas DataFrame
-        DataFrame with optimal weights and associated ticker
-    '''
-    
-    print("Start computation...")
-    # Maximization of sharpe ratio
-    result_sr = max_sharpe_ratio(returns, ret_cov, risk_free_rate)
-    # Compute the return of our portfolio
-    ret_prt_sr, vol_prt_sr = portfolio(result_sr['x'], returns, ret_cov)
-    print("Maximization of sharpe ratio done")
+        # Store data
+        frontier_vol[ 0] = vol_prt_mv 
+        frontier_vol[-1] = vol_prt_sr
+        data["0"]        = [round(i*100, 2) for i in result_sr['x']]
+        data[f"{N_P-1}"] = [round(i*100, 2) for i in result_mv['x']]
 
-    # Minimization of volatility
-    result_mv = min_vol(returns, ret_cov)
-    # Compute the return of our portfolio
-    ret_prt_mv, vol_prt_mv = portfolio(result_mv['x'], returns, ret_cov)
-    print("Minimization of volatility done")
+        # Frontier's computation
+        target = np.linspace(ret_prt_mv, ret_prt_sr, N_P)
 
-    # Frontier
-    frontier_vol = []
-    target       = np.linspace(ret_prt_mv, ret_prt_sr, N_P)
-    
-    # Dataframe to store all porfolios
-    data = pd.DataFrame(index=ticker, columns=[f"{i}" for i in range(N_P)])
-    
-    print("Start frontier computation...")
-    # computation of frontier
-    for i, t in enumerate(target):
-        tmp = frontier(returns, ret_cov, t)
-        frontier_vol.append(tmp['fun'])
-        data[f"{i}"] = [round(i*100, 2) for i in tmp['x']]
-        print(f"{(i+1)/N_P * 100:.2f} % \r", end="")
-    
-    # percentage return
-    return 100*np.array(frontier_vol), 100*target, data
+        for i, t in enumerate(target[1:-1]):
 
+            # Unitarity constraint and return constraint
+            constraints = ({'type': 'eq', 'fun': lambda w: np.sum(w) - 1.},
+                           {'type': 'eq', 'fun': lambda w: self.ret_portfolio(w) - t})
+            tmp = self.minimum(self.vol_portfolio, constraints)
+            # Store data
+            frontier_vol[i+1] = tmp['fun']
+            data[f"{i+1}"] = [round(j*100, 2) for j in tmp['x']]
+
+        frontier_vol, target = 100*frontier_vol, 100*target
+
+        return frontier_vol, target, data
 
 if __name__ == '__main__':
 
@@ -293,8 +220,9 @@ if __name__ == '__main__':
     # Pre-loaded data
     #==============================================================================
     """
-    n_asset    = 50   # number of asset in our portfolio (max 431 for return_2.npy)
+    n_asset    = 5   # number of asset in our portfolio (max 431 for return_2.npy)
     n_days     = 1006   # number of day (max 1005 for return_2.npy)
+    days_in_yr = 252
     
     list_tk = np.load('data/indici_2.npy', allow_pickle='TRUE')
     list_tk = list_tk[0:n_asset]
@@ -320,9 +248,6 @@ if __name__ == '__main__':
         # Compute the return to 1 day
         adj_close = history[ticker]['Adj Close']
         return_1d.append(adj_close.pct_change()[1:])
-        # normalizzation
-        #sig = np.sqrt(np.mean(return_1d[i]**2)-np.mean(return_1d[i])**2)
-        #return_1d[i] = (return_1d[i] - np.mean(return_1d[i]))/sig 
 
     return_1d  = np.array(return_1d)
     cross_corr = np.cov(return_1d)
@@ -332,11 +257,12 @@ if __name__ == '__main__':
     # Computation
     #==============================================================================
 
-    frontier_vol, frontier_ret, data = get_result(return_1d, cross_corr, list_tk)
+    portfolio = Portfolio(return_1d, cross_corr, list_tk)
+    frontier_vol, frontier_ret, data = portfolio.optimization(20)
     data = data[data > 0].dropna(how='all')
-    
+
     data.to_csv('data/portfolios.csv', index=True)
-    
+
     #==============================================================================
     # Plot Efficiet frontier
     #==============================================================================
@@ -348,7 +274,8 @@ if __name__ == '__main__':
     for i in range(n_portfolios):
         w = np.random.rand(cross_corr.shape[0]) # All w_i > 0
         w = w / sum(w)                          # sum(W) must be 1
-        ret[i], vol[i] = portfolio(w, return_1d, cross_corr)
+        ret[i] = sum( w * np.mean(return_1d, axis=1)) * days_in_yr
+        vol[i] = np.sqrt((w.T @ cross_corr @ w) * days_in_yr)
 
     sr = ret/vol # sharpe ratio
 
