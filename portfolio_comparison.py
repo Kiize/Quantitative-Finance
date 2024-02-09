32debf0c7e4bbee1878a621860cc6cace6a239b7
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2024-02-09 18:38:00 +0100

    new version

--- portfolio_comparison.py
+++ portfolio_comparison.py
@@ -1,3 +1,6 @@
+"""
+Code to meake a comparison between portfolios
+"""
 import time 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,57 +10,64 @@ import utilities as ut
 
 start = time.time()
 
-n_asset = 200  # number of asset in our portfolio (max 431 for return_2.npy)
+#==============================================================================
+# Load data
+#==============================================================================
 
-# 2022.
+n_asset     = 200  # number of asset in our portfolio (max 431 for 2022.npy)
+target_name = "2022"
+input_name  = "2023"
 
-list_tk_2022 = np.load('data/indici_16_19.npy', allow_pickle='TRUE')
-list_tk_2022 = list_tk_2022[0:n_asset]
+# --------------------------- 2022 ---------------------------
 
-return_1d_2022 = np.load('data/return_16_19.npy', allow_pickle='TRUE')
-return_1d_2022 = return_1d_2022[0:n_asset, :]
+list_tk_input    = np.load(f'data/indici_{input_name}.npy', allow_pickle='TRUE')
+list_tk_input    = list_tk_input[0:n_asset]
 
-cross_corr_2022 = np.load('data/cross_correlation_16_19.npy', allow_pickle='TRUE')
-cross_corr_2022 = cross_corr_2022[0:n_asset, 0:n_asset]
+return_1d_input  = np.load(f'data/return_{input_name}.npy', allow_pickle='TRUE')
+return_1d_input  = return_1d_input[0:n_asset, :]
 
-filt_corr_2022 = ut.c_filtering(cross_corr_2022, return_1d_2022)
+cross_corr_input = np.load(f'data/cross_correlation_{input_name}.npy', allow_pickle='TRUE')
+cross_corr_input = cross_corr_input[0:n_asset, 0:n_asset]
 
-sigma_2022 = np.array([np.sqrt(np.mean(return_1d_2022[i, :]**2)-np.mean(return_1d_2022[i, :])**2) for i in range(n_asset)])
+filtr_corr_input = ut.c_filtering(cross_corr_input, return_1d_input)
 
-cross_corr_2022 = ut.q_operator(cross_corr_2022, sigma_2022)
-filt_corr_2022  = ut.q_operator(filt_corr_2022, sigma_2022)
+sigma_input = np.array([np.sqrt(np.mean(return_1d_input[i, :]**2)-np.mean(return_1d_input[i, :])**2) for i in range(n_asset)])
 
+cross_corr_input = ut.q_operator(cross_corr_input, sigma_input)
+filtr_corr_input = ut.q_operator(filtr_corr_input, sigma_input)
 
-# 2023.
+# --------------------------- 2023 ---------------------------
 
-list_tk_2023 = np.load('data/indici_20_23.npy', allow_pickle='TRUE')
-list_tk_2023 = list_tk_2023[0:n_asset]
+list_tk_target    = np.load(f'data/indici_{target_name}.npy', allow_pickle='TRUE')
+list_tk_target    = list_tk_target[0:n_asset]
 
-return_1d_2023 = np.load('data/return_20_23.npy', allow_pickle='TRUE')
-return_1d_2023 = return_1d_2023[0:n_asset, :]
+return_1d_target  = np.load(f'data/return_{target_name}.npy', allow_pickle='TRUE')
+return_1d_target  = return_1d_target[0:n_asset, :]
 
-cross_corr_2023 = np.load('data/cross_correlation_20_23.npy', allow_pickle='TRUE')
-cross_corr_2023 = cross_corr_2023[0:n_asset, 0:n_asset]
+cross_corr_target = np.load(f'data/cross_correlation_{target_name}.npy', allow_pickle='TRUE')
+cross_corr_target = cross_corr_target[0:n_asset, 0:n_asset]
 
-sigma_2023 = np.array([np.sqrt(np.mean(return_1d_2023[i, :]**2)-np.mean(return_1d_2023[i, :])**2) for i in range(n_asset)])
+sigma_target = np.array([np.sqrt(np.mean(return_1d_target[i, :]**2)-np.mean(return_1d_target[i, :])**2) for i in range(n_asset)])
 
-cross_corr_2023 = ut.q_operator(cross_corr_2023, sigma_2023)
+cross_corr_target = ut.q_operator(cross_corr_target, sigma_target)
 
-
+#==============================================================================
+# Optimizzation
+#==============================================================================
 
 # Portfolio with cross_correlation.
 
-portfolio_2023_real = Portfolio(return_1d_2023, cross_corr_2023, list_tk_2023)
-portfolio_2023_pred = Portfolio(return_1d_2023, cross_corr_2022, list_tk_2023)
+portfolio_real = Portfolio(return_1d_target, cross_corr_target, list_tk_target)
+portfolio_pred = Portfolio(return_1d_target, cross_corr_input,  list_tk_input)
 
-vol_pred, ret_pred, _ = portfolio_2023_pred.optimization(20)
-vol_real, ret_real, _ = portfolio_2023_real.optimization(20)
+vol_pred, ret_pred, _ = portfolio_pred.optimization(20)
+vol_real, ret_real, _ = portfolio_real.optimization(20)
 
 # Portfolio with filtered correlation.
 
-portfolio_2023_pred_filt = Portfolio(return_1d_2023, filt_corr_2022, list_tk_2023)
+portfolio_pred_filtr = Portfolio(return_1d_target, filtr_corr_input, list_tk_input)
 
-vol_pred_filt, ret_pred_filt, _ = portfolio_2023_pred_filt.optimization(20)
+vol_pred_filtr, ret_pred_filtr, _ = portfolio_pred_filtr.optimization(20)
 
 # Elapsed time.
 
@@ -75,7 +85,7 @@ print(f"Elapsed time: {mins} min {sec:.2f} sec")
 plt.figure(1)
 plt.plot(vol_pred, ret_pred, "r-", label="Predicted")
 plt.plot(vol_real, ret_real, "b-", label="Realized")
-plt.plot(vol_pred_filt, ret_pred_filt, "y--", label="Predicted filtered")
+plt.plot(vol_pred_filtr, ret_pred_filtr, "y--", label="Predicted filtered")
 
 plt.title("Portfolio optimization comparison", fontsize=10)
 plt.xlabel('Volatility %', fontsize=10)
@@ -85,6 +95,3 @@ plt.legend(loc='best')
 plt.grid()
 
 plt.show()   
-
-
-
